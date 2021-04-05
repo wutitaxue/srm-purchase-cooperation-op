97872382d3c9ce57f38a7e92465fa349a28b7623
yantao <tao.yan04@hand-china.com>
2021-04-05 22:45:36 +0800

    [add]提交远程分支

--- src/main/java/org/srm/autoconfigure/purchasecooperation/SrmPurchaseCooperationOPAutoConfiguration.java
+++ src/main/java/org/srm/autoconfigure/purchasecooperation/SrmPurchaseCooperationOPAutoConfiguration.java
@@ -1,8 +1,15 @@
 package org.srm.autoconfigure.purchasecooperation;
 
+import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
+import org.hzero.boot.platform.entity.autoconfigure.EntityRegistScan;
+import org.hzero.core.jackson.annotation.EnableObjectMapper;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.srm.boot.autoconfigure.EnableSrmEvent;
 import org.srm.purchasecooperation.autoconfigure.EnableSrmPurchaseCooperation;
 
 /**
@@ -11,8 +18,22 @@ import org.srm.purchasecooperation.autoconfigure.EnableSrmPurchaseCooperation;
  * @author zhongsheng.xu@hand-china.com 2021-03-23 20:18:33
  */
 @Configuration
-@EnableFeignClients("org.srm.purchasecooperation.cux")
-@ComponentScan("org.srm.purchasecooperation.cux")
-@EnableSrmPurchaseCooperation
+//@EnableFeignClients("org.srm.purchasecooperation.cux")
+@ComponentScan({"org.srm.purchasecooperation.cux","org.srm.purchasecooperation.cux.infra.mapper"})
+//@EnableSrmPurchaseCooperation
+
+@ComponentScan({"org.srm.purchasecooperation.asn", "org.srm.purchasecooperation.finance", "org.srm.purchasecooperation.forecast", "org.srm.purchasecooperation.order", "org.srm.purchasecooperation.pr", "org.srm.purchasecooperation.transaction", "org.srm.purchasecooperation.ssch", "org.srm.purchasecooperation.plan", "org.srm.purchasecooperation.budget", "org.srm.purchasecooperation.sinv", "org.srm.boot.platform.print", "org.srm.purchasecooperation.accept", "org.srm.purchasecooperation.common", "org.srm.source", "org.srm.purchasecooperation.utils"})
+@EnableFeignClients({"org.srm.purchasecooperation.asn", "org.srm.purchasecooperation.finance", "org.srm.purchasecooperation.order", "org.srm.purchasecooperation.pr", "org.srm.purchasecooperation.transaction", "org.srm.purchasecooperation.plan", "org.srm.purchasecooperation.budget", "org.srm.purchasecooperation.sinv", "org.srm.source.priceLib", "io.choerodon", "org.srm.source","org.srm.purchasecooperation.cux"})
+//@EnableChoerodonResourceServer
+@EnableDiscoveryClient
+@EnableAsync
+@EnableAspectJAutoProxy(
+        exposeProxy = true
+)
+@EnableObjectMapper
+@EnableSrmEvent
+@EntityRegistScan(
+        basePackages = {"org.srm.purchasecooperation/asn/domain/entity/**"}
+)
 public class SrmPurchaseCooperationOPAutoConfiguration {
 }
---
+++ src/main/java/org/srm/purchasecooperation/cux/app/service/RCWLTaxInvoiceLineService.java
@@ -0,0 +1,19 @@
+package org.srm.purchasecooperation.cux.app.service;
+
+
+
+import org.srm.purchasecooperation.cux.domain.entity.InvoiceData;
+import org.srm.purchasecooperation.cux.domain.entity.ResponseData;
+import java.util.List;
+
+public interface RCWLTaxInvoiceLineService {
+    /**
+     * 税务发票信息同步
+     *
+     * @param tenantId
+     * @param invoiceDataList
+     */
+    int InvoiceSynchronization(Long tenantId, List<InvoiceData> invoiceDataList);
+
+    int ResponseData(ResponseData responseData);
+}
