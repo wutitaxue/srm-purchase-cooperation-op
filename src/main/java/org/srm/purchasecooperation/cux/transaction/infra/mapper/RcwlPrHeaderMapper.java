49e32a0b18511b809054eb49a86fbafd4c6085c3
李明豪 <minghao.li@hand-china.com>
2021-04-07 13:49:53 +0800

    修改类的继承

--- src/main/java/org/srm/purchasecooperation/cux/transaction/infra/mapper/RcwlPrHeaderMapper.java
+++ src/main/java/org/srm/purchasecooperation/cux/transaction/infra/mapper/RcwlPrHeaderMapper.java
@@ -1,9 +1,8 @@
 package org.srm.purchasecooperation.cux.transaction.infra.mapper;
 
-import org.srm.purchasecooperation.pr.infra.mapper.PrHeaderMapper;
 import org.srm.purchasecooperation.sinv.app.service.dto.PcHeaderDetailDTO;
 
-public interface RcwlPrHeaderMapper extends PrHeaderMapper {
+public interface RcwlPrHeaderMapper {
     PcHeaderDetailDTO selectPcHeaderDetailByPcNum(String pcNum, Long tenantId);
 
 }
