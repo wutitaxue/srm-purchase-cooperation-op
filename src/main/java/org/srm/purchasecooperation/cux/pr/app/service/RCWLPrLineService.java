cd9500d4f0b529001aae2d34dda2b9f766ec2451
zhangbin <bin.zhang06@hand-china.com>
2021-04-13 18:27:36 +0800

    采购计划目录迁移

---
+++ src/main/java/org/srm/purchasecooperation/cux/pr/app/service/RCWLPrLineService.java
@@ -0,0 +1,10 @@
+package org.srm.purchasecooperation.cux.pr.app.service;
+
+/**
+ * 采购申请行应用服务
+ *
+ * @author bin.zhang06@hand-china.com 2021-03-16 15:49:15
+ */
+public interface RCWLPrLineService {
+
+}
