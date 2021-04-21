f8ed90f2431f04d281d2ab2c8dc41725b0da705a
yongbo.ji <yongbo.ji@hand-china.com>
2021-04-21 19:13:01 +0800

    考评档案维护-标准功能修改-后端 2021.4.20 2.3版本:修改service

--- src/main/java/org/srm/purchasecooperation/cux/sinv/infra/mapper/RcwlSinvRcvTrxHeaderMapper.java
+++ src/main/java/org/srm/purchasecooperation/cux/sinv/infra/mapper/RcwlSinvRcvTrxHeaderMapper.java
@@ -1,9 +1,10 @@
 package org.srm.purchasecooperation.cux.sinv.infra.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.srm.purchasecooperation.cux.sinv.domain.vo.SinvRcvTrxToKpiAutoPOLineVO;
 import org.srm.purchasecooperation.sinv.api.dto.SinvRcvTrxHeaderDTO;
 
 public interface RcwlSinvRcvTrxHeaderMapper {
 
-    SinvRcvTrxToKpiAutoPOLineVO countTrxHeaderByClosedFlag (SinvRcvTrxHeaderDTO sinvRcvTrxHeaderDTO);
+    SinvRcvTrxToKpiAutoPOLineVO countTrxHeaderByClosedFlag (@Param("sinvRcvTrxHeaderDTO") SinvRcvTrxHeaderDTO sinvRcvTrxHeaderDTO);
 }
