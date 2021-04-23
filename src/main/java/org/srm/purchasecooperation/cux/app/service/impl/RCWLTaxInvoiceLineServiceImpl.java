0986ea8543a6c9c46f7082a9084797bb9565bd1c
Tao Yan <tao.yan04@hand-china.com>
2021-04-23 18:17:31 +0800

    [add]提交远程分支02-23-1326

--- src/main/java/org/srm/purchasecooperation/cux/app/service/impl/RCWLTaxInvoiceLineServiceImpl.java
+++ src/main/java/org/srm/purchasecooperation/cux/app/service/impl/RCWLTaxInvoiceLineServiceImpl.java
@@ -44,7 +44,7 @@ public class RCWLTaxInvoiceLineServiceImpl implements RCWLTaxInvoiceLineService 
                 List list = this.rcwlTaxInvoiceLineRepository.selectByCondition(
                         Condition.builder(TaxInvoiceLine.class)
                                 .andWhere(Sqls.custom()
-                                        .andEqualTo(TaxInvoiceLine.FIELD_TAX_INVOICE_LINE_ID, invoiceHeader.getInvoiceHeaderId())
+                                        .andEqualTo(TaxInvoiceLine.FIELD_INVOICE_HEADER_ID, invoiceHeader.getInvoiceHeaderId())
                                 ).build());
                 if(list.size()==0){
                     log.info("====================塞值=========================");
