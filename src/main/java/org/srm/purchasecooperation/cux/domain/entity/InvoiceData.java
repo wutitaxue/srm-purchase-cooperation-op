f803913d588d770b73d624d8d279f31d1e17fcda
Tao Yan <tao.yan04@hand-china.com>
2021-04-25 10:51:31 +0800

    [add]提交远程分支02-25-10-51

--- src/main/java/org/srm/purchasecooperation/cux/domain/entity/InvoiceData.java
+++ src/main/java/org/srm/purchasecooperation/cux/domain/entity/InvoiceData.java
@@ -1,5 +1,6 @@
 package org.srm.purchasecooperation.cux.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -8,17 +9,29 @@ import java.util.Date;
 
 @Data
 public class InvoiceData {
+    @JsonProperty("documentType")
     private String documentType;
+    @JsonProperty("documentNumber")
     private String documentNumber;
+    @JsonProperty("invoiceCode")
     private String invoiceCode;
+    @JsonProperty("invoiceNumber")
     private String invoiceNumber;
+    @JsonProperty("invoiceTypeCode")
     private String invoiceTypeCode;
     @DateTimeFormat(pattern = "yyyy-MM-dd hh24:mm:ss")
+    @JsonProperty("billingDate")
     private Date billingDate;
+    @JsonProperty("totalAmount")
     private BigDecimal totalAmount;
+    @JsonProperty("taxAmount")
     private BigDecimal taxAmount;
+    @JsonProperty("taxIncludedAmount")
     private BigDecimal taxIncludedAmount;
+    @JsonProperty("taxIncludedStatusCode")
     private String taxIncludedStatusCode;
+    @JsonProperty("validateStatus")
     private String validateStatus;
+    @JsonProperty("checkCode")
     private String checkCode;
 }
