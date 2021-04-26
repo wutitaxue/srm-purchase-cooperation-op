c7ef784614bf0cefd1eae59c8a8b3f4ba5fc0a68
yongbo.ji <yongbo.ji@hand-china.com>
2021-04-26 13:56:54 +0800

    新增“供应商考评”按钮 2021.4.26 1.1版本:

--- src/main/java/org/srm/purchasecooperation/cux/order/infra/mapper/RcwlMyPoHeaderMapper.java
+++ src/main/java/org/srm/purchasecooperation/cux/order/infra/mapper/RcwlMyPoHeaderMapper.java
@@ -2,6 +2,6 @@ package org.srm.purchasecooperation.cux.order.infra.mapper;
 
 import org.srm.purchasecooperation.order.api.dto.PoHeaderDetailDTO;
 
-public interface RcwlPoHeaderMapper {
+public interface RcwlMyPoHeaderMapper {
     PoHeaderDetailDTO rcwlSelectHeaderdetail(Long tenantId, Long poHeaderId);
 }
--- src/main/java/org/srm/purchasecooperation/cux/order/infra/repository/impl/RcwlMyPoHeaderRepositoryImpl.java
+++ src/main/java/org/srm/purchasecooperation/cux/order/infra/repository/impl/RcwlMyPoHeaderRepositoryImpl.java
@@ -3,21 +3,21 @@ package org.srm.purchasecooperation.cux.order.infra.repository.impl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.srm.purchasecooperation.cux.order.infra.mapper.RcwlPoHeaderMapper;
+import org.srm.purchasecooperation.cux.order.infra.mapper.RcwlMyPoHeaderMapper;
 import org.srm.purchasecooperation.order.api.dto.PoHeaderDetailDTO;
 import org.srm.purchasecooperation.order.infra.repository.impl.PoHeaderRepositoryImpl;
 import org.srm.web.annotation.Tenant;
 
 @Component
 @Tenant("SRM-RCWL")
-public class RcwlPoHeaderRepositoryImpl extends PoHeaderRepositoryImpl {
+public class RcwlMyPoHeaderRepositoryImpl extends PoHeaderRepositoryImpl {
 
     @Autowired
-    private RcwlPoHeaderMapper rcwlPoHeaderMapper;
+    private RcwlMyPoHeaderMapper rcwlMyPoHeaderMapper;
 
     @Override
     public PoHeaderDetailDTO selectHeaderdetail(Long tenantId, Long poHeaderId) {
-        PoHeaderDetailDTO poHeaderDetailDTO = this.rcwlPoHeaderMapper.rcwlSelectHeaderdetail(tenantId, poHeaderId);
+        PoHeaderDetailDTO poHeaderDetailDTO = this.rcwlMyPoHeaderMapper.rcwlSelectHeaderdetail(tenantId, poHeaderId);
         PoHeaderDetailDTO poHeaderDetailDTO1 = this.selectHeaderdetailAdress(tenantId, poHeaderId);
         if (poHeaderDetailDTO1 != null) {
             if (StringUtils.isNotEmpty(poHeaderDetailDTO1.getShipToLocContName())) {
