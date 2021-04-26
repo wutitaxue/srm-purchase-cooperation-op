43c2595f175dbd99b645c757106807b0d5025987
yongbo.ji <yongbo.ji@hand-china.com>
2021-04-26 18:30:48 +0800

    新增“供应商考评”按钮 2021.4.26 1.3版本:

--- src/main/java/org/srm/purchasecooperation/cux/order/infra/mapper/RcwlMyPoHeaderMapper.java
+++ src/main/java/org/srm/purchasecooperation/cux/order/infra/mapper/RcwlMyPoHeaderMapper.java
@@ -1,12 +1,8 @@
 package org.srm.purchasecooperation.cux.order.infra.mapper;
 
-import org.srm.purchasecooperation.order.api.dto.PoHeaderDetailDTO;
-import org.srm.purchasecooperation.order.domain.entity.PoHeader;
-
-import java.util.List;
 
 public interface RcwlMyPoHeaderMapper {
-    PoHeaderDetailDTO rcwlSelectHeaderdetail(Long tenantId, Long poHeaderId);
 
-    List<PoHeader> rcwlSelectPoHeader(PoHeader poHeader);
+    String rcwlSelect (Long poHeaderId);
+
 }
