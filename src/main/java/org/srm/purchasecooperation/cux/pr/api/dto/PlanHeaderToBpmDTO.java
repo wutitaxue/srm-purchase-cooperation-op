0e27783a8466a09769142153e88048dccdcdeee0
zhangbin <bin.zhang06@hand-china.com>
2021-04-27 17:24:07 +0800

    提交添加bpm接口

--- src/main/java/org/srm/purchasecooperation/cux/pr/api/dto/PlanHeaderToBpmDTO.java
+++ src/main/java/org/srm/purchasecooperation/cux/pr/api/dto/PlanHeaderToBpmDTO.java
@@ -1,10 +1,15 @@
 package org.srm.purchasecooperation.cux.pr.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @description:
  * @author: bin.zhang
  * @createDate: 2021/4/27 17:12
  */
 public class PlanHeaderToBpmDTO {
+   @JsonProperty("FSubject")
+   @ApiModelProperty(value = "标题")
    private  String FSubject;
 }
