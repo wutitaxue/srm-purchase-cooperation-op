6c676c062084ae5bb59ca56871188d40a8ba0b13
zhangbin <bin.zhang06@hand-china.com>
2021-04-28 17:02:13 +0800

    采购计划提交接口

--- src/main/java/org/srm/purchasecooperation/cux/pr/api/controller/v1/RCWLPlanHeaderController.java
+++ src/main/java/org/srm/purchasecooperation/cux/pr/api/controller/v1/RCWLPlanHeaderController.java
@@ -17,6 +17,7 @@ import org.hzero.export.annotation.ExcelExport;
 import org.hzero.export.vo.ExportParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.srm.purchasecooperation.cux.pr.api.dto.HeaderQueryDTO;
 import org.srm.purchasecooperation.cux.pr.api.dto.PlanHeaderExportDTO;
@@ -90,18 +91,21 @@ public class RCWLPlanHeaderController extends BaseController {
         RCWLPlanHeaderService.saveAttachment(tenantId, planHeader);
         return Results.success();
     }
-
+    @Transactional(
+            rollbackFor = {Exception.class}
+    )
     @ApiOperation(value = "批量取消采购计划")
     @Permission(level = ResourceLevel.ORGANIZATION)
 //   @Permission(permissionPublic = true)
-
     @PostMapping("/cancel")
     public ResponseEntity<List<PlanHeader>> batchCancelPlanHeader(@ApiParam(value = "租户id", required = true) @PathVariable(value = "organizationId") Long organizationId,
                                                                   @ApiParam(value = "采购计划表list") @RequestBody List<PlanHeader> planHeaderList) {
 
         return Results.success(RCWLPlanHeaderService.batchCancelPlanHeader(organizationId, planHeaderList));
     }
-
+    @Transactional(
+            rollbackFor = {Exception.class}
+    )
     @ApiOperation(value = "创建保存采购计划")
     @Permission(level = ResourceLevel.ORGANIZATION)
     //@Permission(permissionPublic = true)
@@ -112,7 +116,9 @@ public class RCWLPlanHeaderController extends BaseController {
         return Results.success();
     }
 
-
+    @Transactional(
+            rollbackFor = {Exception.class}
+    )
     @ApiOperation(value = "提交采购计划")
     @Permission(level = ResourceLevel.ORGANIZATION)
     //@Permission(permissionPublic = true)
