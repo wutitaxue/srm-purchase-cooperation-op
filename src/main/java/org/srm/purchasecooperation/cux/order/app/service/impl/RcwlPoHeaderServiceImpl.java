2a530fda234ca23418855e4fc6bd65eab2ebf2c8
SpringZZY <519915774@qq.com>
2021-05-28 11:16:17 +0800

    opd-26 需求修改 取值修改

--- src/main/java/org/srm/purchasecooperation/cux/order/app/service/impl/RcwlPoHeaderServiceImpl.java
+++ src/main/java/org/srm/purchasecooperation/cux/order/app/service/impl/RcwlPoHeaderServiceImpl.java
@@ -719,7 +719,7 @@ public class RcwlPoHeaderServiceImpl extends PoHeaderServiceImpl {
 
                 // opd-26
                 if(null != poLine.getPrLineId()){
-                    poLine.setAttributeVarchar21(String.valueOf(rcwlSpcmPcSubjectRepository.queryPrLineByKey(poLine.getPrLineId()).get(0).get("budget_account_id")));
+                    poLine.setAttributeVarchar21(String.valueOf(rcwlSpcmPcSubjectRepository.queryPrLineByKey(poLine.getPrLineId()).get(0).get("budget_account_num")));
                 }
                    LOGGER.info("srm-22875-poLineRepository.insertSelective-dete{}", poLine);
                 this.poLineRepository.insertSelective(poLine);
