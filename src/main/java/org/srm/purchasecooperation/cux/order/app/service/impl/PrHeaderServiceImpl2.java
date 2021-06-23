9467abe500a2ea0190afabd0f15df5b6b142b89b
yongbo.ji <yongbo.ji@hand-china.com>
2021-06-23 16:53:05 +0800

    bugfix 6.23 商城采购申请生成物料编码 1.3

--- src/main/java/org/srm/purchasecooperation/cux/order/app/service/impl/PrHeaderServiceImpl2.java
+++ src/main/java/org/srm/purchasecooperation/cux/order/app/service/impl/PrHeaderServiceImpl2.java
@@ -9,6 +9,7 @@ import io.choerodon.core.oauth.DetailsHelper;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hzero.boot.customize.util.CustomizeHelper;
+import org.hzero.boot.platform.code.builder.CodeRuleBuilder;
 import org.hzero.core.base.BaseConstants;
 import org.hzero.mybatis.domian.Condition;
 import org.hzero.mybatis.util.Sqls;
@@ -20,14 +21,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.srm.boot.platform.configcenter.CnfHelper;
 import org.srm.purchasecooperation.common.api.dto.TenantDTO;
+import org.srm.purchasecooperation.cux.order.domain.repository.RcwlPoHeaderRepository;
+import org.srm.purchasecooperation.cux.order.domain.vo.RCWLItemInfoVO;
+import org.srm.purchasecooperation.cux.pr.infra.mapper.RcwlCheckPoLineMapper;
 import org.srm.purchasecooperation.cux.pr.infra.mapper.RcwlPrFeignMapper;
 import org.srm.purchasecooperation.order.api.dto.PoDTO;
 import org.srm.purchasecooperation.order.app.service.PoHeaderService;
+import org.srm.purchasecooperation.order.domain.entity.PoLine;
+import org.srm.purchasecooperation.order.domain.repository.PoLineRepository;
 import org.srm.purchasecooperation.order.infra.mapper.PoHeaderMapper;
 import org.srm.purchasecooperation.pr.app.service.PrHeaderService;
 import org.srm.purchasecooperation.pr.app.service.impl.PrHeaderServiceImpl;
+import org.srm.purchasecooperation.pr.domain.entity.ItemCategoryAssign;
 import org.srm.purchasecooperation.pr.domain.entity.PrHeader;
 import org.srm.purchasecooperation.pr.domain.entity.PrLine;
+import org.srm.purchasecooperation.pr.domain.repository.ItemCategoryAssignRepository;
 import org.srm.purchasecooperation.pr.domain.repository.PrLineRepository;
 import org.srm.purchasecooperation.utils.annotation.EventSendTran;
 
@@ -49,6 +57,16 @@ public class PrHeaderServiceImpl2 {
     private PoHeaderMapper poHeaderMapper;
     @Autowired
     private RcwlPrFeignMapper rcwlPrFeignMapper;
+    @Autowired
+    private PoLineRepository poLineRepository;
+    @Autowired
+    private RcwlCheckPoLineMapper rcwlCheckPoLineMapper;
+    @Autowired
+    private RcwlPoHeaderRepository poHeaderRepository;
+    @Autowired
+    private CodeRuleBuilder codeRuleBuilder;
+    @Autowired
+    private ItemCategoryAssignRepository itemCategoryAssignRepository;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PrHeaderServiceImpl.class);
 
@@ -186,6 +204,101 @@ public class PrHeaderServiceImpl2 {
                                     throw new CommonException(var15, new Object[0]);
                                 }
                             }
+                            //bugfix-0623-jyb start
+                            //校验po行商品编号有无物料编码
+                            PoLine poLine = new PoLine();
+                            poLine.setPoHeaderId(poDTO.getPoHeaderId());
+                            poLine.setTenantId(tenantId);
+                            List<PoLine> PoLines = poLineRepository.select(poLine);
+                            List<RCWLItemInfoVO> poLineList = new ArrayList<>();
+                            PoLines.forEach(itemLine-> {
+                                RCWLItemInfoVO rcwlItemInfoVO = rcwlCheckPoLineMapper.checkPoItem(itemLine.getProductNum(), itemLine.getTenantId());
+                                //没有物料创建编码并插入订单
+                                if (rcwlItemInfoVO == null) {
+                                    //查询需要封装的item数据集合(排除存在物料id的数据)
+                                    List listTemp = new ArrayList();
+                                    listTemp.add(itemLine.getPoLineId());
+                                    List<RCWLItemInfoVO> rcwlItemInfoVOList = poHeaderRepository.selectItemListByPoLineIdList(listTemp, tenantId);
+                                    List<ItemCategoryAssign> itemCategoryAssignList = new ArrayList<>();
+                                    // 生成对应条编码
+                                    if (CollectionUtils.isNotEmpty(rcwlItemInfoVOList)) {
+                                        //判断来自哪个电商平台
+                                        String dsFlag = poHeaderRepository.selectShopMallSupplier(poDTO.getPoHeaderId(),tenantId);
+
+                                        rcwlItemInfoVOList.forEach(item -> {
+                                            String itemCode = null;
+                                            String categoryCode = item.getItemCode();
+                                            //电商来源的物料编码为 品类编码+来源（00，01，02等）+电商编码
+                                            if("CG".equals(dsFlag)){
+                                                itemCode = categoryCode+"04"+item.getProductNum();
+                                            }else if("ZKH".equals(dsFlag)){
+                                                itemCode = categoryCode+"03"+item.getProductNum();
+                                            }else if("JD".equals(dsFlag)){
+                                                itemCode = categoryCode+"02"+item.getProductNum();
+                                            }else{
+                                                String str = this.codeRuleBuilder.generateCode("SODR.RCWL.ITEM_CODE",  (Map) null);
+                                                itemCode = categoryCode+str;
+                                            }
+
+                                            //物料设值
+                                            item.setTenantId(tenantId);
+                                            //String categoryCode = item.getItemCode();
+                                            // String str = this.codeRuleBuilder.generateCode(DetailsHelper.getUserDetails().getTenantId(), "SODR.RCWL.ITEM_CODE", "GLOBAL", "GLOBAL", (Map) null);
+                                            String ruleCode = this.codeRuleBuilder.generateCode("SMDM.ITEM", (Map) null);
+                                            item.setItemNumber(ruleCode);
+                                            item.setItemCode(itemCode);
+                                            item.setQueryItemCode(itemCode);
+                                            item.setSpecifications(item.getSpecifications());
+                                            item.setModel(item.getModel());
+                                        });
+                                        //批量插入物料表
+                                        poHeaderRepository.batchInsertItem(rcwlItemInfoVOList);
+
+                                        List<Long> lineIds = rcwlItemInfoVOList.stream().map(RCWLItemInfoVO::getPoLineId).distinct().collect(Collectors.toList());
+                                        //查询出需要封装好的itemCategoryAssign
+                                        List<RCWLItemInfoVO> itemCategoryList = poHeaderRepository.selectItemCategoryListByPoLineIdList(lineIds, tenantId);
+
+                                        if (CollectionUtils.isNotEmpty(itemCategoryList)) {
+                                            itemCategoryList.forEach(item -> {
+                                                ItemCategoryAssign itemCategoryAssign = new ItemCategoryAssign();
+                                                itemCategoryAssign.setItemId(item.getItemId());
+                                                itemCategoryAssign.setTenantId(tenantId);
+                                                itemCategoryAssign.setSourceCode(item.getSourceCode());
+                                                itemCategoryAssign.setCategoryId(item.getCategoryId());
+                                                itemCategoryAssignList.add(itemCategoryAssign);
+                                            });
+                                            //批量插入物料分配品类表
+                                            itemCategoryAssignRepository.batchInsertSelective(itemCategoryAssignList);
+                                        }
+                                        //把item_id item_code回写到订单行
+                                        List<RCWLItemInfoVO> poLineList1 = new ArrayList<>();
+                                        if (CollectionUtils.isNotEmpty(itemCategoryList)) {
+                                            itemCategoryList.forEach(poLine1 -> {
+                                                RCWLItemInfoVO lineUpdateInfo = new RCWLItemInfoVO();
+                                                lineUpdateInfo.setTenantId(tenantId);
+                                                lineUpdateInfo.setItemId(poLine1.getItemId());
+                                                lineUpdateInfo.setItemCode(poLine1.getItemCode());
+                                                lineUpdateInfo.setPoLineId(poLine1.getPoLineId());
+                                                lineUpdateInfo.setItemName(poLine1.getItemName());
+                                                poLineList1.add(lineUpdateInfo);
+                                            });
+                                            //批量更新订单物料id和code
+                                            poHeaderRepository.batchUpdatePoLine(poLineList1);
+                                            //批量插入物料名称多语言表smdm_item_tl
+                                            poHeaderRepository.batchInsertItemTl(poLineList1);
+                                        }
+                                    }
+                                    //存在则只插入订单行物料编码和物料id物料名称
+                                }else {
+                                    rcwlItemInfoVO.setPoLineId(itemLine.getPoLineId());
+                                    poLineList.add(rcwlItemInfoVO);
+                                }
+                            });
+                            if (poLineList.size() > 0) {
+                                //批量更新订单物料id和code
+                                poHeaderRepository.batchUpdatePoLine(poLineList);
+                            }
+                            //bugfix-0623-jyb end
                         }
                     }
 
