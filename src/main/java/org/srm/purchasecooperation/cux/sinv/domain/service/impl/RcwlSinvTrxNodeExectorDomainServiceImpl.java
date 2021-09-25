0a7b87606b9e0e2e7b40fcbff16593403308334e
zhangbin <bin.zhang06@hand-china.com>
2021-09-25 13:55:23 +0800

    由送货单过来的执行金额保留两位小数

--- src/main/java/org/srm/purchasecooperation/cux/sinv/domain/service/impl/RcwlSinvTrxNodeExectorDomainServiceImpl.java
+++ src/main/java/org/srm/purchasecooperation/cux/sinv/domain/service/impl/RcwlSinvTrxNodeExectorDomainServiceImpl.java
@@ -6,14 +6,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
 import org.srm.purchasecooperation.order.infra.convertor.CommonConvertor;
 import org.srm.purchasecooperation.sinv.api.dto.SinvRcvTrxHeaderDTO;
 import org.srm.purchasecooperation.sinv.api.dto.SinvRcvTrxLineDTO;
 import org.srm.purchasecooperation.sinv.domain.entity.RcvStrategyLine;
+import org.srm.purchasecooperation.sinv.domain.entity.SinvRcvTrxHeader;
 import org.srm.purchasecooperation.sinv.domain.entity.SinvRcvTrxLine;
+import org.srm.purchasecooperation.sinv.domain.entity.SinvRcvTrxOrderLink;
 import org.srm.purchasecooperation.sinv.domain.repository.SinvRcvTrxLineRepository;
 import org.srm.purchasecooperation.sinv.domain.service.impl.SinvTrxNodeExectorDomainServiceImpl;
+import org.srm.purchasecooperation.sinv.domain.vo.InitSinvRcvTrxDataVO;
+import org.srm.purchasecooperation.sinv.domain.vo.SinvRcvTrxOrderLinkVO;
 import org.srm.purchasecooperation.sinv.infra.mapper.SinvRcvTrxHeaderMapper;
+import org.srm.purchasecooperation.sinv.infra.mapper.SinvRcvTrxLineMapper;
 import org.srm.web.annotation.Tenant;
 
 import java.math.BigDecimal;
@@ -36,7 +43,8 @@ public class RcwlSinvTrxNodeExectorDomainServiceImpl extends SinvTrxNodeExectorD
     private SinvRcvTrxHeaderMapper sinvRcvTrxHeaderMapper;
     @Autowired
     private SinvRcvTrxLineRepository sinvRcvTrxLineRepository;
-
+    @Autowired
+    private SinvRcvTrxLineMapper sinvRcvTrxLineMapper;
     @Override
     public SinvRcvTrxHeaderDTO plusQuantityOccupy(Long tenantId, SinvRcvTrxHeaderDTO sinvRcvTrxHeaderDTO) {
         LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-plusQuantityOccupy:begin");
@@ -102,5 +110,53 @@ public class RcwlSinvTrxNodeExectorDomainServiceImpl extends SinvTrxNodeExectorD
 
         this.sinvRcvTrxLineRepository.updateOptional(fromSinvRcvTrxLine, new String[]{"occupiedQuantity", "occupiedTaxAmount", "completeFlag"});
         LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-plusQuantitySingleLineOccupy:end");
+    }
+
+    @Override
+    public List<SinvRcvTrxLine> insertSinvLine(List<InitSinvRcvTrxDataVO> receiveRcvTrxDataVOS, SinvRcvTrxHeader rcvTrxHeader, Long tenantId) {
+        LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-insertSinvLine:begin");
+        LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-insertSinvLine:receiveRcvTrxDataVOS{}", receiveRcvTrxDataVOS);
+        LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-insertSinvLine:rcvTrxHeader{}", rcvTrxHeader);
+        List<SinvRcvTrxLine> rcvTrxLines = new ArrayList();
+        Integer initialNodeFlag = ((InitSinvRcvTrxDataVO)receiveRcvTrxDataVOS.get(0)).getInitialNodeFlag();
+
+        for(int i = 0; i < receiveRcvTrxDataVOS.size(); ++i) {
+            InitSinvRcvTrxDataVO receiveRcvTrxDataVO = (InitSinvRcvTrxDataVO)receiveRcvTrxDataVOS.get(i);
+            SinvRcvTrxLine rcvTrxLine = (SinvRcvTrxLine)CommonConvertor.beanConvert(SinvRcvTrxLine.class, receiveRcvTrxDataVO);
+            rcvTrxLine.setRcvTrxHeaderId(rcvTrxHeader.getRcvTrxHeaderId());
+            rcvTrxLine.setTrxYear(rcvTrxHeader.getTrxYear());
+            rcvTrxLine.setOccupiedTaxAmount(BigDecimal.ZERO);
+            rcvTrxLine.setReversedTaxAmount(BigDecimal.ZERO);
+            rcvTrxLine.setOccupiedQuantity(BigDecimal.ZERO);
+            rcvTrxLine.setReversedQuantity(BigDecimal.ZERO);
+            //来源为订单和送货单的执行金额统一保留两位小数
+            BigDecimal taxIncludeAmount = receiveRcvTrxDataVO.getTaxIncludedAmount().setScale(2,RoundingMode.HALF_UP);
+            rcvTrxLine.setTaxIncludedAmount(taxIncludeAmount);
+            if (BaseConstants.Flag.YES.equals(initialNodeFlag)) {
+                rcvTrxLine.setRcvTrxTypeId(0L);
+            } else if (ObjectUtils.isEmpty(receiveRcvTrxDataVO.getRcvTrxTypeId())) {
+                Long rcvTrxTypeId = this.sinvRcvTrxLineMapper.selectRcvTrxTypeId(tenantId, rcvTrxHeader.getNodeConfigId(), "SRM", (String)null);
+                rcvTrxLine.setRcvTrxTypeId(rcvTrxTypeId);
+            }
+
+            Assert.notNull(rcvTrxLine.getRcvTrxTypeId(), "rcv.trx.type.code.not.exists");
+            rcvTrxLine.setStockType("OWN_STOCK");
+            rcvTrxLine.setTrxLineNum((long)(i + 1));
+            rcvTrxLine.setDisplayTrxLineNum((String)Optional.ofNullable(rcvTrxLine.getDisplayTrxLineNum()).orElse(String.valueOf(rcvTrxLine.getTrxLineNum())));
+            rcvTrxLine.setFromPoLineLocationId(receiveRcvTrxDataVO.getPoLineLocationId());
+            if (ObjectUtils.isEmpty(rcvTrxLine.getOrderLinkId())) {
+                SinvRcvTrxOrderLinkVO sinvRcvTrxOrderLinkVO = receiveRcvTrxDataVO.getSinvRcvTrxOrderLinksVO();
+                SinvRcvTrxOrderLink sinvRcvTrxOrderLink = this.insertSinvOrderLink(tenantId, sinvRcvTrxOrderLinkVO, receiveRcvTrxDataVO);
+                rcvTrxLine.setOrderLinkId(sinvRcvTrxOrderLink.getOrderLinkId());
+            }
+
+            rcvTrxLine.setOrgQuantity(rcvTrxLine.getQuantity());
+            rcvTrxLines.add(rcvTrxLine);
+        }
+
+        this.sinvRcvTrxLineRepository.batchInsertSelective(rcvTrxLines);
+        LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-insertSinvLine:rcvTrxLines", rcvTrxLines);
+        LOGGER.info("srm-22587-SinvRcvTrxDomainServiceImpl-insertSinvLine:begin");
+        return rcvTrxLines;
     }
 }
