12805b55f6bb29823a842a070fc3089c7011c8f1
zhangbin <bin.zhang06@hand-china.com>
2021-09-27 11:44:14 +0800

    收货执行修复数据

--- src/main/java/org/srm/purchasecooperation/cux/pr/api/controller/v1/RCWLPlanHeaderController.java
+++ src/main/java/org/srm/purchasecooperation/cux/pr/api/controller/v1/RCWLPlanHeaderController.java
@@ -140,8 +140,8 @@ public class RCWLPlanHeaderController extends BaseController {
         return Results.success();
     }
     @ApiOperation(value = "收货执行父行数据修复")
-    @Permission(level = ResourceLevel.ORGANIZATION)
-    //@Permission(permissionPublic = true)
+   // @Permission(level = ResourceLevel.ORGANIZATION)
+    @Permission(permissionPublic = true)
     @PostMapping("/data-fix-1")
     public ResponseEntity dataFix(@PathVariable Long organizationId){
         this.RCWLPlanHeaderService.fixDataOne(organizationId);
