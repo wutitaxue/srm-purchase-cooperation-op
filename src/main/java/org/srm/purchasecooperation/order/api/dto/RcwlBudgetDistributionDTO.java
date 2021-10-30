4a2047c0f3f3595a20db35b8c350be8b66049ad2
zpx <912432457@qq.com>
2021-10-30 11:39:38 +0800

    [add]rcwl-group-76 新增订单跨年预算 2021-10-30 zpx

--- src/main/java/org/srm/purchasecooperation/order/api/dto/RcwlBudgetDistributionDTO.java
+++ src/main/java/org/srm/purchasecooperation/order/api/dto/RcwlBudgetDistributionDTO.java
@@ -20,7 +20,26 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RcwlBudgetDistributionDTO extends RcwlBudgetDistribution {
+public class RcwlBudgetDistributionDTO {
+
+    @ApiModelProperty("表ID，主键")
+    private Long budgetLineId;
+    @ApiModelProperty(value = "订单头id,sodr_po_header.po_header_id")
+    private Long poHeaderId;
+    @ApiModelProperty(value = "订单行id,sodr_po_line,po_line_id")
+    private Long poLineId;
+    @ApiModelProperty(value = "申请头id,sprm_pr_header.pr_header_id")
+    private Long prHeaderId;
+    @ApiModelProperty(value = "申请行id,sprm_pr_line.pr_line_id")
+    private Long prLineId;
+    @ApiModelProperty(value = "预算占用年份")
+    private Long budgetDisYear;
+    @ApiModelProperty(value = "预算占用金额（四舍五入）")
+    private BigDecimal budgetDisAmount;
+    @ApiModelProperty(value = "预算总时长（月）")
+    private Long budgetDisGap;
+    @ApiModelProperty(value = "租户id")
+    private Long tenantId;
 
     @ApiModelProperty(value = "物料编码")
     private String itemCode;
