e66c693815353a35b605bfab209747a77d01ef83
zpx <912432457@qq.com>
2021-10-30 12:07:55 +0800

    [add]rcwl-group-76 新增订单跨年预算 2021-10-30 zpx

--- src/main/java/org/srm/purchasecooperation/order/api/controller/v1/RcwlBudgetDistributionController.java
+++ src/main/java/org/srm/purchasecooperation/order/api/controller/v1/RcwlBudgetDistributionController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +37,7 @@ public class RcwlBudgetDistributionController extends BaseController {
 
     @ApiOperation(value = "根据订单行生成并获取预算分配列表")
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @GetMapping("/list")
+    @PostMapping("/list")
     public ResponseEntity<List<RcwlBudgetDistribution>> create(@PathVariable(value = "organizationId") long tenantId, RcwlBudgetDistributionDTO rcwlBudgetDistributionDTO) {
         List<RcwlBudgetDistribution> rcwlBudgetDistributions = rcwlBudgetDistributionService.selectBudgetDistributionByPoLine(tenantId, rcwlBudgetDistributionDTO);
         return Results.success(rcwlBudgetDistributions);
