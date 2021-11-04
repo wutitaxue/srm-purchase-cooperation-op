e8d016f329748f9b973273540a6add8f5a3e3e8f
王杰 <jie.wang05@hand-china.com>
2021-11-04 16:36:55 +0800

    [FIX]opd-5682 采购申请订单提交预算逻辑判断修改 2021-11-04 王杰

--- src/main/java/org/srm/purchasecooperation/order/app/service/impl/RcwlBudgetDistributionServiceImpl.java
+++ src/main/java/org/srm/purchasecooperation/order/app/service/impl/RcwlBudgetDistributionServiceImpl.java
@@ -247,8 +247,8 @@ public class RcwlBudgetDistributionServiceImpl implements RcwlBudgetDistribution
             });
             // 总体逻辑:已有跨年预算,删除->重新插入.  对于校验的部分,提交那里会限制住
             if (!org.springframework.util.CollectionUtils.isEmpty(budgetDistributions)) {
-                // 判断系统分摊金额总值和实际分摊金额总值是否相等,不相等---可能是人为调整有问题,或者金额变化了
-                if (!rcwlBudgetDistributionDTOS.stream().map(RcwlBudgetDistributionDTO::getAutoCalculateBudgetDisAmount).reduce(BigDecimal.ZERO, BigDecimal::add).equals(rcwlBudgetDistributionDTOS.stream().map(RcwlBudgetDistributionDTO::getBudgetDisAmount).reduce(BigDecimal.ZERO, BigDecimal::add))) {
+                // 判断行金额和实际分摊金额总值是否相等,不相等---可能是人为调整有问题,或者金额变化了
+                if (!rcwlBudgetDistributionDTOS.get(0).getLineAmount().equals(rcwlBudgetDistributionDTOS.stream().map(RcwlBudgetDistributionDTO::getBudgetDisAmount).reduce(BigDecimal.ZERO, BigDecimal::add))) {
                     // 判断跨年分摊金额表中实际分摊金额总值和传入的跨年分摊金额的实际分摊金额总值是否相等,不相等说明人为调整有误,报错
                     if (!rcwlBudgetDistributionDTOS.stream().map(RcwlBudgetDistributionDTO::getBudgetDisAmount).reduce(BigDecimal.ZERO, BigDecimal::add).equals(budgetDistributions.stream().map(RcwlBudgetDistribution::getBudgetDisAmount).reduce(BigDecimal.ZERO, BigDecimal::add))) {
                         throw new CommonException("error.pr.line.amount.budget.error");
