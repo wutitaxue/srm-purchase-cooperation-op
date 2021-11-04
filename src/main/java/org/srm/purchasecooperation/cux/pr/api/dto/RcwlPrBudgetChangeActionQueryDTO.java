9e21d9ca5a20200347c33d4281e1fa586e8653da
zpx <912432457@qq.com>
2021-11-04 19:53:20 +0800

    [feat]rcwl-group-76 代码测试 2021-11-04 zpx

---
+++ src/main/java/org/srm/purchasecooperation/cux/pr/api/dto/RcwlPrBudgetChangeActionQueryDTO.java
@@ -0,0 +1,28 @@
+package org.srm.purchasecooperation.cux.pr.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.srm.purchasecooperation.cux.pr.domain.entity.RcwlBudgetChangeAction;
+import org.srm.purchasecooperation.pr.domain.entity.PrLine;
+
+import java.util.List;
+
+/**
+ * @author jie.wang05@hand-china.com 2021/11/4 17:42
+ * @description 采购申请变更DTO
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RcwlPrBudgetChangeActionQueryDTO {
+    /**
+     * 采购申请变更预算集合
+     */
+    private List<RcwlBudgetChangeAction> rcwlBudgetChangeActions;
+
+    /**
+     * 采购申请行
+     */
+    private PrLine prLine;
+}
