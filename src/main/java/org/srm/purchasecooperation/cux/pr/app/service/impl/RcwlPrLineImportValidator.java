019989378b96a3f2a15bb670bc7e3ccd6788c06d
黄佳兴33042 <wutitaxue@gmail.com>
2021-11-12 09:46:47 +0800

    [ADD]opd-6190 模板导入时系统自动根据采购申请头上的公司给相应公司下的库存组织作为默认值。

--- src/main/java/org/srm/purchasecooperation/cux/pr/app/service/impl/RcwlPrLineImportValidator.java
+++ src/main/java/org/srm/purchasecooperation/cux/pr/app/service/impl/RcwlPrLineImportValidator.java
@@ -5,10 +5,12 @@ import io.choerodon.core.oauth.DetailsHelper;
 import org.apache.commons.lang3.StringUtils;
 import org.hzero.boot.imported.infra.validator.annotation.ImportValidator;
 import org.hzero.boot.imported.infra.validator.annotation.ImportValidators;
+import org.hzero.core.base.BaseConstants;
 import org.hzero.core.message.MessageAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.srm.purchasecooperation.cux.pr.domain.vo.RcwlPrLineImportVO;
+import org.srm.purchasecooperation.cux.pr.infra.constant.Constants;
 import org.srm.purchasecooperation.cux.pr.infra.mapper.RcwlPrImportMapper;
 import org.srm.purchasecooperation.cux.sinv.infra.util.TenantValue;
 import org.srm.purchasecooperation.pr.app.service.impl.PrLineImportValidator;
@@ -52,9 +54,9 @@ public class RcwlPrLineImportValidator extends PrLineImportValidator {
 
         Long tenantId = DetailsHelper.getUserDetails().getTenantId();
         Long prHeaderId = Long.valueOf(this.getArgs().get("prHeaderId").toString());
-        PrHeader prHeader = (PrHeader)this.prHeaderRepository.selectByPrimaryKey(prHeaderId);
+        PrHeader prHeader = this.prHeaderRepository.selectByPrimaryKey(prHeaderId);
         if (prHeader == null && !prHeader.getTenantId().equals(tenantId)) {
-            this.addErrorMsg("error.data_invalid", prLineImportVO, (String)null);
+            this.addErrorMsg("error.data_invalid", prLineImportVO, null);
             return false;
         } else if (StringUtils.isNotEmpty(super.getContext().getErrorMsg())) {
             return false;
@@ -102,42 +104,38 @@ public class RcwlPrLineImportValidator extends PrLineImportValidator {
                 return false;
             }
         }
-        //this.codeValid(prLineImportVO, this.prImportMapper::queryInvOrganizationInfo, prLineImportVO.getInvOrganizationCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("invOrganizationCode"), "sprm.pr_line_import.inv_organization_error", true);
-        //this.codeValid(prLineImportVO, this.prImportMapper::queryInventoryInfo, prLineImportVO.getInventoryCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("inventoryCode"), "sprm.pr_line_import.inventory_code_error", false);
         if (!prLineImportVO.isValidFlag()) {
             return false;
         } else {
             if (StringUtils.isEmpty(prLineImportVO.getItemCode())) {
                 if (StringUtils.isEmpty(prLineImportVO.getItemName())) {
-                    this.addErrorMsg("sprm.pr_line_import.item_code_and_name_be_lost_error", prLineImportVO, (String)null);
+                    this.addErrorMsg("sprm.pr_line_import.item_code_and_name_be_lost_error", prLineImportVO, null);
                     return false;
                 }
 
-                this.codeValid(prLineImportVO, this.prImportMapper::queryUomInfo, prLineImportVO.getUomCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("uomCode"), "sprm.pr_line_import.uom_error", true);
+                this.codeValid(prLineImportVO, this.prImportMapper::queryUomInfo, prLineImportVO.getUomCode(), PrLineImportVO.FIELD_NAME_MAP.get("uomCode"), "sprm.pr_line_import.uom_error", true);
             } else {
-                this.codeValid(prLineImportVO, this.prImportMapper::queryItemInfo, prLineImportVO.getItemCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("itemCode"), "sprm.pr_line_import.item_error", false);
+                this.codeValid(prLineImportVO, this.prImportMapper::queryItemInfo, prLineImportVO.getItemCode(), PrLineImportVO.FIELD_NAME_MAP.get("itemCode"), "sprm.pr_line_import.item_error", false);
             }
 
             if (!prLineImportVO.isValidFlag()) {
                 return false;
             } else {
-                this.codeValid(prLineImportVO, this.prImportMapper::queryCategoryInfo, prLineImportVO.getCategoryCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("categoryCode"), "sprm.pr_line_import.category_error", false);
+                this.codeValid(prLineImportVO, this.prImportMapper::queryCategoryInfo, prLineImportVO.getCategoryCode(), PrLineImportVO.FIELD_NAME_MAP.get("categoryCode"), "sprm.pr_line_import.category_error", false);
                 if (!prLineImportVO.isValidFlag()) {
                     return false;
                 } else {
                     if (prLineImportVO.getNeededDate() == null) {
-                        this.addErrorMsg("sprm.pr_line_import.needed_date_error", prLineImportVO, (String)null);
+                        this.addErrorMsg("sprm.pr_line_import.needed_date_error", prLineImportVO, null);
                     }
 
-                    //this.userValid(prLineImportVO, this.prImportMapper::queryUserInfo, prLineImportVO.getRequestedLoginName(), (String)PrLineImportVO.FIELD_NAME_MAP.get("requestedLoginName"), "sprm.pr_line_import.requested_user_error");
                     if (!prLineImportVO.isValidFlag()) {
                         return false;
                     } else {
-                        //this.codeValid(prLineImportVO, this.prImportMapper::queryPurchaseAgent, prLineImportVO.getPurchaseAgentCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("purchaseAgentCode"), "sprm.pr_line_import.purchase_agent_error", false);
                         if (!prLineImportVO.isValidFlag()) {
                             return false;
                         } else {
-                            this.codeValid(prLineImportVO, this.prImportMapper::queryTaxInfo, prLineImportVO.getTaxCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("taxCode"), "sprm.pr_line_import.tax_error", false);
+                            this.codeValid(prLineImportVO, this.prImportMapper::queryTaxInfo, prLineImportVO.getTaxCode(), PrLineImportVO.FIELD_NAME_MAP.get("taxCode"), "sprm.pr_line_import.tax_error", false);
                             if (!prLineImportVO.isValidFlag()) {
                                 return false;
                             } else {
@@ -148,7 +146,7 @@ public class RcwlPrLineImportValidator extends PrLineImportValidator {
                                         if (unitVO == null) {
                                             unitVO = this.prImportMapper.queryUnitInfoUnlimited(prLineImportVO.getTenantId(), prLineImportVO.getExpBearDepUnitCode());
                                             if (unitVO == null) {
-                                                this.addErrorMsg("sprm.pr_line_import.unit_error", prLineImportVO, (String)PrLineImportVO.FIELD_NAME_MAP.get("expBearDepUnitCode"));
+                                                this.addErrorMsg("sprm.pr_line_import.unit_error", prLineImportVO, PrLineImportVO.FIELD_NAME_MAP.get("expBearDepUnitCode"));
                                             }
                                         }
                                     }
@@ -157,28 +155,27 @@ public class RcwlPrLineImportValidator extends PrLineImportValidator {
                                 if (!prLineImportVO.isValidFlag()) {
                                     return false;
                                 } else {
-                                    //this.codeValid(prLineImportVO, this.prImportMapper::querySupplierInfo, prLineImportVO.getAdviseSupplierCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("adviseSupplierCode"), "sprm.pr_line_import.supplier_error", false);
                                     if (!prLineImportVO.isValidFlag()) {
                                         return false;
                                     } else {
-                                        this.codeValid(prLineImportVO, this.prImportMapper::queryCostInfo, prLineImportVO.getCostCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("costCode"), "sprm.pr_line_import.cost_error", false);
+                                        this.codeValid(prLineImportVO, this.prImportMapper::queryCostInfo, prLineImportVO.getCostCode(), PrLineImportVO.FIELD_NAME_MAP.get("costCode"), "sprm.pr_line_import.cost_error", false);
                                         if (!prLineImportVO.isValidFlag()) {
                                             return false;
                                         } else {
-                                            this.codeValid(prLineImportVO, this.prImportMapper::queryAccountSubjectInfo, prLineImportVO.getAccountSubjectNum(), (String)PrLineImportVO.FIELD_NAME_MAP.get("accountSubjectNum"), "sprm.pr_line_import.account_subject_error", false);
+                                            this.codeValid(prLineImportVO, this.prImportMapper::queryAccountSubjectInfo, prLineImportVO.getAccountSubjectNum(), PrLineImportVO.FIELD_NAME_MAP.get("accountSubjectNum"), "sprm.pr_line_import.account_subject_error", false);
                                             if (!prLineImportVO.isValidFlag()) {
                                                 return false;
                                             } else {
-                                                this.userValid(prLineImportVO, this.prImportMapper::queryUserInfo, prLineImportVO.getKeeperUserLoginName(), (String)PrLineImportVO.FIELD_NAME_MAP.get("keeperUserLoginName"), "sprm.pr_line_import.keeper_user_error");
+                                                this.userValid(prLineImportVO, this.prImportMapper::queryUserInfo, prLineImportVO.getKeeperUserLoginName(), PrLineImportVO.FIELD_NAME_MAP.get("keeperUserLoginName"), "sprm.pr_line_import.keeper_user_error");
                                                 if (!prLineImportVO.isValidFlag()) {
                                                     return false;
                                                 } else {
-                                                    this.userValid(prLineImportVO, this.prImportMapper::queryUserInfo, prLineImportVO.getAccepterUserLoginName(), (String)PrLineImportVO.FIELD_NAME_MAP.get("accepterUserLoginName"), "sprm.pr_line_import.accepter_user_error");
+                                                    this.userValid(prLineImportVO, this.prImportMapper::queryUserInfo, prLineImportVO.getAccepterUserLoginName(), PrLineImportVO.FIELD_NAME_MAP.get("accepterUserLoginName"), "sprm.pr_line_import.accepter_user_error");
                                                     if (!prLineImportVO.isValidFlag()) {
                                                         return false;
                                                     } else {
-                                                        this.codeValid(prLineImportVO, this.prImportMapper::queryAccountAssignType, prLineImportVO.getAccountAssignTypeCode(), (String)PrLineImportVO.FIELD_NAME_MAP.get("accountAssignTypeCode"), "sprm.pr_line_import.account_assign_error", false);
-                                                        this.codeValid(prLineImportVO, this.prImportMapper::queryWbs, prLineImportVO.getWbs(), (String)PrLineImportVO.FIELD_NAME_MAP.get("accountAssignTypeCode"), "sprm.pr_line_import.wbs", false);
+                                                        this.codeValid(prLineImportVO, this.prImportMapper::queryAccountAssignType, prLineImportVO.getAccountAssignTypeCode(), PrLineImportVO.FIELD_NAME_MAP.get("accountAssignTypeCode"), "sprm.pr_line_import.account_assign_error", false);
+                                                        this.codeValid(prLineImportVO, this.prImportMapper::queryWbs, prLineImportVO.getWbs(), PrLineImportVO.FIELD_NAME_MAP.get("accountAssignTypeCode"), "sprm.pr_line_import.wbs", false);
                                                         return prLineImportVO.isValidFlag();
                                                     }
                                                 }
